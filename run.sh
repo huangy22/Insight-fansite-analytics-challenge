c63e17f42c8959a76a53b82efd87f8ea505ab928
Yuan <huangy22@gmail.com>
2017-04-05 23:58:41 -0400

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# one example of run.sh script for implementing the features using python
-# the contents of this script could be replaced with similar files from any major language
-
 # I'll execute my programs, with the input directory log_input and output the files in the directory log_output
 if [ $# -ge 1 ]
 then
--- run_geolocation.sh
+++ run_geolocation.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-# one example of run.sh script for implementing the features using python
-# the contents of this script could be replaced with similar files from any major language
-
 # I'll execute my programs, with the input directory log_input and output the files in the directory log_output
-python ./src/geolocation.py ./log_output/test/hosts_sample.txt ./geochart/
+python ./src/geolocation.py ./log_output/hosts_sample.txt ./geochart/
